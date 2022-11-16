bbe4bf5da5a8126616e6855a3c503d1b328353da
Giovanne Oliveira <giovanne@giovanne.dev>
2022-11-16 13:56:59 +0000

    first repo setup

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php
+/*
+ * Arquivo: /jibri_uploader/index.php
+ * Projeto: Plataforma Médico Online
+ * Versão: 1.7.0
+ * Data da criação: Monday, November 14th 2022, 1:31:07 pm
+ * Autor: Giovanne Oliveira
+ * -----
+ * Ultima edição: Mon Nov 14 2022
+ * Editado por: Giovanne Oliveira
+ * -----
+ * Copyright (c) 2022 Plataforma Medico Online - JVM Serviços Médicos Ltda
+ * -----
+ * Este software, bem como todo o seu código fonte e assets são protegidos por Direitos Autorais. Distribuição proibída.
+ * https://medicoonline.med.br
+ * -----
+ */
+
+require_once('vendor/autoload.php');
+
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('jibri_uploader');
+$log->pushHandler(new StreamHandler('logs/jibri_uploader.log', \Monolog\Logger::DEBUG));
+$log->pushHandler(new \Monolog\Handler\LogglyHandler('25ea780c-5b1b-4d0f-b6be-6739e6f4b163/tag/monolog', \Monolog\Logger::DEBUG));
+$log->pushHandler(new \Monolog\Handler\SlackWebhookHandler('https://hooks.slack.com/services/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', \Monolog\Logger::ERROR));
+
+$log->info('Jibri Uploader started', ['args' => $argv]);
+
+
+$recordingPath = $argc > 1 ? $argv[1] : null;
+
+if ($recordingPath == null) {
+    $log->error('No recording path provided', ['args' => $argv]);
+    echo "No recording path provided";
+    exit(1);
+}
+
+$recordingPath = realpath($recordingPath);
+
+if (!file_exists($recordingPath)) {
+    $log->error('Recording path does not exist', ['args' => $argv, 'recordingPath' => $recordingPath]);
+    echo "Recording path does not exist";
+    exit(1);
+}
+
+$metadata = json_decode(file_get_contents($recordingPath . '/metadata.json'), true);
+
+if ($metadata == null) {
+    $log->error('No metadata found', ['args' => $argv, 'recordingPath' => $recordingPath]);
+    echo "No metadata found";
+    exit(1);
+}
+
+$uploadEndpoint = $metadata['participants'][0]['group'];
+
+if ($uploadEndpoint == null) {
+    $log->error('No upload endpoint found', ['args' => $argv, 'recordingPath' => $recordingPath, 'metadata' => $metadata]);
+    echo "No upload endpoint found";
+    exit(1);
+}
+
+$videoFileName = glob($recordingPath . '/*.mp4')[0];
+
+if ($videoFileName == null) {
+    $log->error('No video file found', ['args' => $argv, 'recordingPath' => $recordingPath, 'metadata' => $metadata]);
+    echo "No video file found";
+    exit(1);
+}
+
+
+$client = new Client();
+
+try {
+    $log->debug('Uploading video', ['args' => $argv, 'recordingPath' => $recordingPath, 'metadata' => $metadata, 'videoFileName' => $videoFileName, 'uploadEndpoint' => $uploadEndpoint]);
+    $request = $client->request('POST', $uploadEndpoint, [
+        'multipart' => [
+            [
+                'name'     => 'file',
+                'contents' => Psr7\Utils::tryFopen($videoFileName, 'r'),
+                'filename' => 'recording.mp4'
+            ]
+        ],
+        'http_errors' => false
+    ]);
+
+    $response = json_decode($request->getBody()->getContents());
+
+    if ($response->status == 200) {
+        $log->info('Video uploaded', ['args' => $argv, 'recordingPath' => $recordingPath, 'metadata' => $metadata, 'videoFileName' => $videoFileName, 'uploadEndpoint' => $uploadEndpoint]);
+        echo "Upload successful";
+        exit(0);
+    }
+} catch (Exception $e) {
+    $log->critical('Error uploading video', ['args' => $argv, 'recordingPath' => $recordingPath, 'metadata' => $metadata, 'videoFileName' => $videoFileName, 'uploadEndpoint' => $uploadEndpoint, 'error' => $e->getMessage()]);
+    $response = json_decode($request->getBody()->getContents());
+    echo "Upload failed";
+    exit(0);
+}
